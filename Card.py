d48bd663a0788aea1118e7919d7c01d482a945c0
sinku <sininoir@gmail.com>
2022-03-14 17:16:06 +0330

    Add new features

--- Card.py
+++ Card.py
@@ -1,5 +1,22 @@
-class Card:
-    def __init__(self, name, description, type) -> None:
+import pygame
+
+CARD_WIDTH = 100
+CARD_HEIGHT = 150
+
+
+class Card(pygame.sprite.Sprite):
+    def __init__(self, name, description, type, image_path=None) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        if (image_path):
+            image = pygame.image.load(image_path).convert_alpha()
+            image = pygame.transform.scale(image, [CARD_WIDTH, CARD_HEIGHT])
+            self.image = image
         self.name = name
         self.description = description
         self.type = type
+
+    def get_line(self):
+        return f"{self.name}: {self.description}"
+
+    def draw_to_board(self, screen, position):
+        screen.blit(self.image, position + [CARD_WIDTH, CARD_HEIGHT])
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from lib2to3 import pygram
-
 import pygame
+from Abilities import Abilities
+from BattleCards import BattleCards
 from Deck import Deck
+from Card import CARD_WIDTH, CARD_HEIGHT
 
 from Player import Player
 from Colors import Colors
@@ -13,9 +14,20 @@ class Board:
 
     def __init__(self) -> None:
         pygame.init()
-        self.player = Player()
         self.screen = pygame.display.set_mode((Board.WIDTH, Board.HEIGHT))
-        self.deck = Deck(self.screen, [200,300], [100,200], "Deck.png")
+        self.player = Player()
+        self.components = []
+        self.components.append(
+            Deck(
+                self.screen,
+                [self.WIDTH - CARD_WIDTH - 20, 20],
+                [CARD_WIDTH, CARD_HEIGHT],
+                "Deck.png",
+            )
+        )
+        self.components.append(Abilities(self.screen, [500, 200], [100, 300]))
+        self.components.append(BattleCards(self.screen, [20, self.HEIGHT - CARD_HEIGHT - 20]))
+        # self.c
         self.screen.fill(Colors.GRAY)
 
     def handle_events(self):
@@ -23,12 +35,8 @@ class Board:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            self.deck.update(event)
-            
-            
-    
-    def draw(self):
-        pass
+            for component in self.components:
+                component.update(event)
 
     def run_game(self):
         while True:
@@ -36,5 +44,5 @@ class Board:
             pygame.display.update()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     Board().run_game()
