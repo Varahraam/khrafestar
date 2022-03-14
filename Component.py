440eb93c659c24087362f26b4f1f01443f2d28f3
sinku <sininoir@gmail.com>
2022-03-14 17:15:39 +0330

    Add component

--- Component.py
+++ Component.py
@@ -1,6 +1,7 @@
 import pygame
 
-class ClickableObject:
+
+class Component:
     def __init__(self, position, size):
         self.position = position
         self.size = size
@@ -8,9 +9,12 @@ class ClickableObject:
     def is_clicked(self, event):
         mouse = pygame.mouse.get_pos()
         if event.type == pygame.MOUSEBUTTONUP:
-            if self.position[0] < mouse[0] < self.position[0] + self.size[0] and self.position[1] < mouse[1] < self.position[1] + self.size[1]:
+            if (
+                self.position[0] < mouse[0] < self.position[0] + self.size[0]
+                and self.position[1] < mouse[1] < self.position[1] + self.size[1]
+            ):
                 return True
         return False
-    
+
     def get_dimensions(self):
         return self.position + self.size
